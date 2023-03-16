5a2d928e99fdca45309ddfa0b5dcdd3e1b5e857d
Augustine Mavor-Parker <augustine.mavor-parker@cs.ucl.ac.uk>
2023-03-16 20:24:54 +0000

    getting there...

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from flax.training.train_state import TrainState
 
 from utils.helpers import load_config
 from utils.models import CategoricalSeparateMLP, build_RND_models, get_model_ready
+import gymnax
 
 
 @pytest.fixture
@@ -24,24 +25,29 @@ def config():
 
 
 @pytest.fixture
-def env():
-    from utils.envs import make_env
-
-    env = make_env("FourRooms-v0")
-    return env
+def gymnax_env(config):
+    env, env_params = gymnax.make(
+        config.train_config.env_name, **config.train_config.env_kwargs
+    )
+    return env, env_params
 
 
 @pytest.fixture
-def PPO(config, env, observation):
+def PPO(config, gymnax_env, observation):
 
+    env, env_params = gymnax_env
     rng = jax.random.PRNGKey(0)
     PPO_model = CategoricalSeparateMLP(
-        **config.network_config,
+        **config.train_config.network_config,
         num_output_units=env.num_actions,
         scale=1,
         novelty_switch=int(1e4),
     )
-    PPO_params = PPO_model.init(rng, observation, PPO_model.initialize_carry(), rng=rng)
+    obs_shape = env.observation_space(env_params).shape
+    counts_shape = env.get_counts().shape
+    PPO_params = PPO_model.init(
+        rng, jnp.zeros(obs_shape), jnp.zeros(counts_shape), jnp.array([0, 0]), rng=rng,
+    )
     return PPO_model, PPO_params
 
 
@@ -64,12 +70,15 @@ def observation():
 
 
 @pytest.fixture
-def rollout_manager(env, config, PPO):
+def rollout_manager(gymnax_env, config, PPO):
     from utils.ppo import RolloutManager
 
     PPO_model, _ = PPO
     rollout_manager = RolloutManager(
-        PPO_model, config.env_name, config.env_kwargs, config.env_params
+        PPO_model,
+        config.train_config.env_name,
+        config.train_config.env_kwargs,
+        config.train_config.env_params,
     )
     return rollout_manager
 
@@ -94,9 +103,10 @@ def test_compute_novelty(RND, observation):
     assert jnp.allclose(novelty, jnp.zeros_like(novelty)) == False
 
 
-def test_log_value_predictions(RND, PPO_model, PPO_train_state, rollout_manager):
+def test_log_value_predictions(RND, PPO, rollout_manager):
     from utils.ppo import log_value_predictions, compute_novelty
 
+    PPO_model, PPO_params = PPO
     RND_model, RND_params, distiller_model, distiller_params = RND
     novelty_signal = compute_novelty(
         observation, RND_model, RND_params, distiller_model, distiller_params
